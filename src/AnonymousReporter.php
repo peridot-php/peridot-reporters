3cf7b121678eabf1fbb7fa3044ad76c706db8068
Brian Scaturro <scaturrob@gmail.com>
2015-04-08 09:26:16 -0400

    initial commit

---
+++ src/AnonymousReporter.php
@@ -0,0 +1,45 @@
+<?php
+namespace Peridot\Reporter;
+
+use Evenement\EventEmitterInterface;
+use Peridot\Configuration;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * The AnonymousReporter creates a reporter from a PHP callable.
+ *
+ * @package Peridot\Reporter
+ */
+class AnonymousReporter extends AbstractBaseReporter
+{
+    /**
+     * @var callable
+     */
+    protected $initFn;
+
+    /**
+     * Creates a reporter from a callable
+     *
+     * @param callable $init
+     * @param OutputInterface $output
+     * @param EventEmitterInterface $eventEmitter
+     */
+    public function __construct(
+        callable $init,
+        OutputInterface $output,
+        EventEmitterInterface $eventEmitter
+    ) {
+        $this->initFn = $init;
+        parent::__construct($output, $eventEmitter);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return void
+     */
+    public function init()
+    {
+        call_user_func($this->initFn, $this);
+    }
+}
